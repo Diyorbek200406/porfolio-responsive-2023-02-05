2989a8e59f37822bcc11d4cd1d86b7d12e9b0b2d
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2023-02-05 22:38:43 +0500

    navigation

--- JS/app.js
+++ JS/app.js
@@ -5,9 +5,16 @@ window.addEventListener("DOMContentLoaded", function () {
   });
   const menuBtn = document.querySelector(".menu-btn");
   const navigation = document.querySelector(".navigation");
+  const navigationItems = document.querySelectorAll(".navigation a");
 
   menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     navigation.classList.toggle("active");
+  });
+  navigationItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+      menuBtn.classList.remove("active");
+      navigation.classList.remove("active");
+    });
   });
 });
